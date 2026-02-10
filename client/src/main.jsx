1bc364f8b62912e9d61ad328e685c9713683ed8c
ADITHYARAMAN7 <adithyaraman0407@gmail.com>
2026-02-10 23:29:20 +0530

    docs: Add comments to main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,8 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// Entry point of the React application
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    {/* Main App Component wrapped in StrictMode for development checks */}
     <App />
   </StrictMode>,
 )
